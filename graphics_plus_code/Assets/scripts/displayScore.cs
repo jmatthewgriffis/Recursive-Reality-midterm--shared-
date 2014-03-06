1a41f08813dcc50d47065d27d1388606f7472c61
J. Matthew Griffis <grifj153@newschool.edu>
2014-03-05 19:05:38 -0500

    Integrated the graphics and code into a working prototype of the dream sequence.

---
+++ graphics_plus_code/Assets/scripts/displayScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayScore : MonoBehaviour {
+	
+	GUIText myText;
+	public int iScore;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myText.text = "Score: " + iScore;
+	}
+}
---
+++ graphics_plus_code/Assets/scripts/eatCheeto.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eatCheeto : MonoBehaviour {
+	
+	Transform scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		scoreDisplay = GameObject.Find("GuiDrawer").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider mouth) {
+		//Debug.Log("yes!");
+		scoreDisplay.GetComponent<displayScore>().iScore++;
+		Destroy(this);
+	}
+}
