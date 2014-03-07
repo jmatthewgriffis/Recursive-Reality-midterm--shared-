b0523bc64a0d77b93cdf27b2de9ecfbfeb36bf21
sabotai <sabotai@gmail.com>
2014-03-07 15:05:15 -0500

    in class updates

---
+++ Cheetos Project/Assets/Scripts/textureScale.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+	void Update() {
+		float scaleX = Mathf.Cos(Time.time) * 0.5F + 1;
+		float scaleY = Mathf.Sin(Time.time) * 0.5F + 1;
+		renderer.material.SetTextureScale("_MainTex", new Vector2(scaleX, scaleY));
+	}
+}
